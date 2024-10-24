22dadfeb20d429ec0ef9a8b6c3ce79267c1241e8
LuisP <luispala96@gmail.com>
2024-10-24 10:39:49 -0600

    Cambio a los colores ligero

--- Departamento.java
+++ Departamento.java
@@ -1,13 +1,13 @@
 class Departamento{
-    
-    public final String BLACK = "\u001B[30m";
-    public final String RED = "\u001B[31m";
-    public final String GREEN = "\u001B[32m";
-    public final String YELLOW = "\u001B[33m";
-    public final String BLUE = "\u001B[34m";
-    public final String PURPLE = "\u001B[35m";
-    public final String CYAN = "\u001B[36m";
-    public final String WHITE = "\u001B[37m";
+    public final String[] colores = {
+    /* BLACK =  */ "\u001B[30m",
+    /* RED =  */ "\u001B[31m",
+    /* GREEN =  */ "\u001B[32m",
+    /* YELLOW = */  "\u001B[33m",
+    /* BLUE =  */ "\u001B[34m",
+    /* PURPLE = */ "\u001B[35m",
+    /* CYAN =  */ "\u001B[36m",
+    /* WHITE =  */ "\u001B[37m"};
 
     public Roomie[] residentes = new Roomie[3];
     public Electrodomestico[] appliances;
@@ -22,9 +22,9 @@ class Departamento{
 
         // Creacion de los residentes de la vivienda
         residentes = new Roomie[] {
-            new Estudiante("Juan", appliances, BLACK),
-            new Estudiante("Pedro", appliances, RED),
-            new Estudiante("Elisa", appliances, GREEN)
+            new Estudiante("Juan", appliances, colores[1]),
+            new Estudiante("Pedro", appliances, colores[2]),
+            new Estudiante("Elisa", appliances, colores[3])
         };
     }
 
